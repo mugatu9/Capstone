2783ca2ebf96d89f7eda1ea32439a83c455fa9d1
Roman <57426099+roman-net@users.noreply.github.com>
2020-04-17 13:57:34 -0700

    Apr17,2020 13.57 RS

---
+++ CSD480Group3Capstone001/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CSD480Group3Capstone001.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
