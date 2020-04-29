df77947100b7f4ea26ef0f9ebd7e55f958eec7cc
Nathan O'Brien <mugatu9@gmail.com>
2020-04-28 17:15:24 -0700

    Created bare bones model, following entity framework specifications

---
+++ CSD480Group3Capstone001/Models/Building.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSD480Group3Capstone001.Models
+{
+    public class Building
+    {
+        public int buildingID { get; set; }
+        public int? tpID { get; set; }
+        public String Address { get; set; }
+        public String? OrgName { get; set; }
+        public TaxParcel TaxParcel { get; set; }
+        public ICollection<Unit> Units { get; set; }
+
+
+
+    }
+}
---
+++ CSD480Group3Capstone001/Models/RentPayment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSD480Group3Capstone001.Models
+{
+    //For Nathan: REMEMBER TO SPECIFY COMPOSITE KEYS IN DB CONTEXT (PK: TenatID, UnitID, StartDate)
+    public class RentPayment
+    {
+        public int TenatID { get; set; }
+        public int UnitID { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public ICollection<Tenat> Tenats { get; set; }
+        public ICollection<Unit> Units { get; set; }
+    }
+}
