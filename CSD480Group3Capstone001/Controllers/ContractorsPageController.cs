21ed996f9dbae4fc72cef7404f8cdc8c57eafd41
Arthur Dawkins <S-Arthur.Dawkins@lwtech.edu>
2020-05-31 12:14:11 -0700

    Added basic support for the contractors page

---
+++ CSD480Group3Capstone001/Controllers/ContractorsPageController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CSD480Group3Capstone001.Models;
+
+namespace CSD480Group3Capstone001.Controllers
+{
+    public class ContractorsPageController : Controller
+    {
+        private readonly ILogger<ContractorsPageController> _logger;
+
+        public ContractorsPageController(ILogger<ContractorsPageController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
